26a7f8aee51414c283093f192ee44c091cca39d6
bruhmagedon <schubert.walk@yandex.ru>
2025-01-21 22:54:07 +0300

    chore: добавил vite конфиг

---
+++ vite.config.ts
@@ -0,0 +1,38 @@
+import path from 'node:path'
+import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
+import svgrPlugin from 'vite-plugin-svgr'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+// https://vite.dev/config/
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react(), svgrPlugin(), tsconfigPaths()],
+    build: {
+      outDir: 'dist',
+      sourcemap: mode !== 'production', // Включаем sourcemap только для dev/stage окружений
+      chunkSizeWarningLimit: 1500, // Строгий лимит для снижения размера chunk
+    },
+    optimizeDeps: {
+      include: ['react', 'react-dom'], // Предварительная загрузка критически важных библиотек
+    },
+    server: {
+      port: 3000,
+      cors: true,
+      open: true,
+    },
+    preview: {
+      port: 3000,
+    },
+    resolve: {
+      alias: {
+        '@app': path.resolve(__dirname, './src/app'),
+        '@pages': path.resolve(__dirname, './src/pages'),
+        '@widgets': path.resolve(__dirname, './src/widgets'),
+        '@shared': path.resolve(__dirname, './src/shared'),
+        '@hooks': path.resolve(__dirname, './src/shared/hooks'),
+        '@ui': path.resolve(__dirname, './src/shared/ui'),
+      },
+    },
+  }
+}) // TODO: add lint-stage
