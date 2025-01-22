cc3a6bcfd6b1f7a1afe398b4b80f7e62d455f664
bruhmagedon <schubert.walk@yandex.ru>
2025-01-23 00:49:49 +0300

    style: базовые scss стили, миксины, переменные и шрифты

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { RouterProvider } from '@app/router/RouterProvider';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
+import '@app/styles/main.scss';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider />
