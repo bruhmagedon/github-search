988c9eb264a4bc33489ac03c9c5b07f4319bfdc5
bruhmagedon <schubert.walk@yandex.ru>
2025-01-25 12:48:19 +0300

    feat: работа над страницей репозитория

--- src/pages/repository-details/RepositoryDetailsPage.tsx
+++ src/pages/repository-details/RepositoryDetailsPage.tsx
@@ -1,12 +1,29 @@
-import { classNames } from '@shared/utilities';
+import { Avatar } from '@common/ui';
+import { classNames } from '@common/utilities';
 
 import cls from './RepositoryDetailsPage.module.scss';
 
 const RepositoryDetailsPage = () => {
   return (
-    <div className={classNames(cls.RepositoryDetailsPage, {}, [])}>
-      Страница репозитория
-    </div>
+    <article className={classNames(cls.RepositoryPage, {}, [])}>
+      <div className={classNames(cls.card, {}, [])}>
+        <h1 className={classNames(cls.cardTitle, {}, [])}>
+          Профиль
+        </h1>
+        <header className={cls.cardHeader}>
+          <Avatar alt='Repostitory Avatar' src='https://avatars.githubusercontent.com/u/1342004?v=4' />
+          <div className={cls.cardTextContainer}>
+            <h2 className={cls.cardRepoName}>tanya-ling/middle-russian-parser</h2>
+            <p className={cls.cardRepoDescription}>Программа для автоматической лемматизации и
+              морфологического анализа русских текстов 15—17 вв
+            </p>
+          </div>
+        </header>
+        <div className={cls.cardStats}>
+          Карточка
+        </div>
+      </div>
+    </article>
   );
 };
 
