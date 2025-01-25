02a23f7d405d1196bd35b407fe52d23740e2bec9
bruhmagedon <schubert.walk@yandex.ru>
2025-01-25 12:45:05 +0300

    feat: изменил папку shared на common

---
+++ src/common/ui/index.ts
@@ -0,0 +1,5 @@
+export { Avatar } from './Avatar/Avatar';
+export { Badge } from './Badge/Badge';
+export { Card } from './Card/Card';
+export { Input } from './Input/Input';
+export { Select } from './Select/Select';
