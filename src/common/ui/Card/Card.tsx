f12ca5ea39418e9827c7233d5ffd0f2a898ef662
bruhmagedon <schubert.walk@yandex.ru>
2025-01-28 22:53:12 +0300

    feat: обновил компонент Card

--- src/common/ui/Card/Card.tsx
+++ src/common/ui/Card/Card.tsx
@@ -1,43 +1,59 @@
-import IconFork from '@assets/Fork.svg?react';
-import IconLike from '@assets/Heart.svg?react';
-import IconLink from '@assets/Link.svg?react';
-import IconStar from '@assets/Star.svg?react';
+import type { IRepository } from '@common/types/Repository';
+
+import IconFork from '@assets/icon/Fork.svg?react';
+import IconLink from '@assets/icon/Link.svg?react';
+import IconStar from '@assets/icon/Star.svg?react';
 import { classNames } from '@common/utilities';
+import { observer } from 'mobx-react-lite';
+import { useNavigate } from 'react-router-dom';
 
 import { Avatar } from '../Avatar/Avatar';
 import { Badge } from '../Badge/Badge';
 import { Button, ButtonTheme } from '../Button/Button';
+import { ToggleFavoriteButton } from '../ToggleFavoriteButton/ToggleFavoriteButton';
 
 import cls from './Card.module.scss';
 
 interface CardProps {
   className?: string;
-  onClick?: () => void;
+  item: IRepository;
 }
 
-export const Card = ({ className, onClick }: CardProps) => {
+export const Card = observer(({ className, item }: CardProps) => {
+  const navigate = useNavigate();
+
+  const onRedirect = () => {
+    navigate(`/repository/${item.owner.login}/${item.name}`);
+  };
+
   return (
     <article className={classNames(cls.card, {}, [className])}>
       <div className={cls.cardContent}>
         <header className={cls.cardContentHeader}>
-          <Avatar alt='Repostitory Avatar' size={32} src='https://avatars.githubusercontent.com/u/1342004?v=4' />
-          <div className={cls.cardBadges} role='group'>
-            <Badge title='234' icon={<IconStar />} />
-            <Badge title='1 329' icon={<IconFork />} />
+          <Avatar alt='Repository Avatar' size={32} src={item.owner.avatar_url} />
+          <div className={cls.cardBadges}>
+            <Badge
+              title={item.stargazers_count.toString()}
+              icon={<IconStar />}
+            />
+            <Badge
+              title={item.forks_count.toString()}
+              icon={<IconFork />}
+            />
           </div>
         </header>
         <div className={cls.cardInfo}>
-          <span className={cls.cardName}>@leveldb</span>
-          <span className={cls.cardFullName}>google/leveldb</span>
+          <span className={cls.cardName}>{`@${item.owner.login}`}</span>
+          <span className={cls.cardFullName}>{item.full_name}</span>
         </div>
       </div>
       <footer className={cls.cardFooter}>
-        <div className={cls.cardFooterButtons} role='group'>
-          <Button square><IconLike /></Button>
+        <div className={cls.cardFooterButtons}>
+          <ToggleFavoriteButton repository={item} />
           <Button square><IconLink /></Button>
         </div>
-        <Button theme={ButtonTheme.RED} onClick={onClick}>Подробнее</Button>
+        <Button theme={ButtonTheme.RED} onClick={onRedirect}>Подробнее</Button>
       </footer>
     </article>
   );
-};
+});
