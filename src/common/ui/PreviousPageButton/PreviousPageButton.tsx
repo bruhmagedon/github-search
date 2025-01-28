541bc8396d8f7344d0deed736453369113170623
bruhmagedon <schubert.walk@yandex.ru>
2025-01-28 21:14:17 +0300

    feat: новый компонент - кнопка для перемещения на предыдущую страницу

---
+++ src/common/ui/PreviousPageButton/PreviousPageButton.tsx
@@ -0,0 +1,26 @@
+import IconArrowLeft from '@assets/icon/ArrowLeft.svg?react';
+import { classNames } from '@common/utilities';
+import { useNavigate } from 'react-router-dom';
+
+import { Button } from '../Button/Button';
+
+import cls from './PreviousPageButton.module.scss';
+
+interface PreviousPageButtonProps {
+  className?: string;
+}
+
+export const PreviousPageButton = ({ className }: PreviousPageButtonProps) => {
+  const navigate = useNavigate();
+
+  return (
+    <div className={classNames(cls.PreviousPageButton, {}, [className])}>
+      <Button onClick={() => navigate(-1)}>
+        <div className={cls.content}>
+          <IconArrowLeft />
+          <span>Back</span>
+        </div>
+      </Button>
+    </div>
+  );
+};
