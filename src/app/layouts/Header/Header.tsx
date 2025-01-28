f030bb97ab92905a233a2fa8af53c03040ee4209
bruhmagedon <schubert.walk@yandex.ru>
2025-01-28 16:30:29 +0300

    feat: добавил ссылки в header и виджет с кол.вом избранных

--- src/app/layouts/Header/Header.tsx
+++ src/app/layouts/Header/Header.tsx
@@ -1,19 +1,42 @@
-import SearchLogo from '@assets/SearchLogo.svg?react';
+import IconAccount from '@assets/icon/Account.svg?react';
+import IconLike from '@assets/icon/Heart.svg?react';
+import SearchLogo from '@assets/icon/SearchLogo.svg?react';
+import { Button, ButtonSize, ButtonTheme, Loader } from '@common/ui';
+import { useNavigate } from 'react-router-dom';
+
+import { FavoritesCounter } from './components/FavoritesCounter';
 
 import cls from './Header.module.scss';
 
-// TODO: Реализовать ссылки
 export const Header = () => {
+  const navigate = useNavigate();
+
   return (
     <header className={cls.header}>
       <div className={cls.headerContainer}>
-        <div className={cls.appLogo}>
+        <div className={cls.appLogo} onClick={() => navigate('/')}>
           <SearchLogo className={cls.appLogoIcon} />
           <span className={cls.appLogoText}>GitHubSearch</span>
         </div>
         <nav className={cls.headerLinksContainer}>
-          <div />
-          <div />
+          <Button
+            square
+            className={cls.profileButton}
+            size={ButtonSize.L}
+            theme={ButtonTheme.GHOST}
+            onClick={() => navigate('/favorites')}
+          >
+            <FavoritesCounter />
+            <IconLike />
+          </Button>
+          <Button
+            square
+            className={cls.profileButton}
+            size={ButtonSize.L}
+            theme={ButtonTheme.HEADER}
+          >
+            <IconAccount />
+          </Button>
         </nav>
       </div>
     </header>
