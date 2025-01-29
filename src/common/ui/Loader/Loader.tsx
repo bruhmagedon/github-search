4e0c94a6a6a5c63292781e47ab21a3ccb4553f09
bruhmagedon <schubert.walk@yandex.ru>
2025-01-29 13:42:51 +0300

    feat: стили и типы loader

--- src/common/ui/Loader/Loader.tsx
+++ src/common/ui/Loader/Loader.tsx
@@ -3,14 +3,21 @@ import { classNames } from '@common/utilities';
 import cls from './Loader.module.scss';
 
 export enum LoaderSize {
+  S = 's',
   M = 'm',
   L = 'l'
 }
 
 interface LoaderProps {
   size?: LoaderSize
+  type?: 'dots' | 'spinner'
 }
 
-export const Loader = ({ size = LoaderSize.L }: LoaderProps) => {
-  return <div className={classNames(cls.loader, {}, [cls[size]])}></div>;
+export const Loader = ({ size = LoaderSize.L, type = 'spinner' }: LoaderProps) => {
+  return (
+    <>
+      {type === 'spinner' && <div className={classNames(cls.loader, {}, [cls[size]])}></div>}
+      {type === 'dots' && <div className={classNames(cls.dots, {}, [cls[size]])}></div>}
+    </>
+  );
 };
