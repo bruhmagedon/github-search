bbce423bc389a1c377f1eed128a1ff86a4d6bcd0
bruhmagedon <schubert.walk@yandex.ru>
2025-01-29 11:30:11 +0300

    feat: предварительный модуль для сортировки

--- src/pages/search/components/SearchSorting/SearchSorting.tsx
+++ src/pages/search/components/SearchSorting/SearchSorting.tsx
@@ -1,20 +1,36 @@
-import { Input, Select } from '@common/ui';
-import { observer } from 'mobx-react-lite';
-import { useState } from 'react';
+import { searchSortVariants } from '@common/config/sortTypesConfig';
+import { rootStore } from '@common/stores/RootStore';
+import { LoaderWrapper, Select, Skeleton } from '@common/ui';
 
 import cls from './SearchSorting.module.scss';
 
-export const SearchSorting = observer(() => {
-  const sortVariants = ['New First', 'Most Stars', 'Most Forks', 'Alphabetical (A-Z)', 'Alphabetical (Z-A)'];
+// TODO ПОКА ЧТО ОТКЛЮЧИЛ OBSERVER ЧТОБЫ ОН НЕ СБРАСЫВАЛ SELECT РЕ-РЕНДЕРОМ
+export const SearchSorting = () => {
+  const { repositoriesState: { data, isLoading }, setSortType } = rootStore.repositoryStore;
 
   const handleSortChange = (selected: string) => {
-    console.log('Selected:', selected);
+    console.log('Selected:', searchSortVariants[selected]);
+    setSortType(searchSortVariants[selected]);
   };
 
   return (
     <div className={cls.sortingContainer}>
-      <div className={cls.sortingResult}>Result: 100 repositories</div>
-      <Select data={sortVariants} onChange={handleSortChange} />
+      <div className={cls.sortingText}>
+        <LoaderWrapper
+          isLoading={isLoading}
+          loader={<Skeleton className={cls.sortingTextSkeleton} />}
+        >
+          <span>Результат: {data?.length || 'Нет результатов'}</span>
+        </LoaderWrapper>
+      </div>
+      <div className={cls.sortingSelect}>
+        <LoaderWrapper
+          isLoading={isLoading}
+          loader={<Skeleton className={cls.sortingSelectSkeleton} />}
+        >
+          <Select data={Object.keys(searchSortVariants)} onChange={handleSortChange} />
+        </LoaderWrapper>
+      </div>
     </div>
   );
-});
+};
